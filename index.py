b10896de4d5d0e75f1e2677b95654bdf5d2539bd
Dahliastar <michndunge05@gmail.com>
2025-08-25 16:42:43 +0300

    Submit assignment

--- index.py
+++ index.py
@@ -1,5 +1,23 @@
-file = open(".WEEK 4/ASSIGNMENT/PYTHON-PROGRAMMING-WEEK-4-ASSIGNMENT/names.pdf", "w")
+# FILE HANDLING
+file = open("./WEEK 4/ASSIGNMENT/names.pdf", "w")
 file.write("Michelle, Margaret\n")
 
-# Reading contents of the file
-file = open(".WEEK 4/ASSIGNMENT/PYTHON-PROGRAMMING-WEEK-4-ASSIGNMENT/names.pdf", "r")
+
+with open("./WEEK 4/ASSIGNMENT/names.pdf", 'r') as file:
+    content = file.read()
+
+modify = content.replace("Michelle", "Nana")
+
+with open("./WEEK 4/ASSIGNMENT/names.pdf", 'w') as file:
+    file.write(modify)
+
+# ERROR HANDLING
+try:
+    file = input("Enter the file name: ")
+    with open(file, 'r') as f:
+        content = f.read()
+        print(content)
+except FileNotFoundError:
+    print("The file does not exist. Please check the file path.")
+except IOError:
+    print("File cannot be read. Please try again.")        
