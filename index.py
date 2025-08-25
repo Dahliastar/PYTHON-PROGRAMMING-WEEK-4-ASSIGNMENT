d7117af4c9dc9817ef53fa74fd4ce235eda25fcd
Dahliastar <michndunge05@gmail.com>
2025-08-25 16:29:06 +0300

    Initial Commit

---
+++ index.py
@@ -0,0 +1,5 @@
+file = open(".WEEK 4/ASSIGNMENT/PYTHON-PROGRAMMING-WEEK-4-ASSIGNMENT/names.pdf", "w")
+file.write("Michelle, Margaret\n")
+
+# Reading contents of the file
+file = open(".WEEK 4/ASSIGNMENT/PYTHON-PROGRAMMING-WEEK-4-ASSIGNMENT/names.pdf", "r")
